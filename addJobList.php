e01cbad202609fe08dda31e0de5830d84a8ca15f
Aya-Hamdan1 <s11821764@stu.najah.edu>
2023-05-18 23:15:33 +0300

    update add job list page

--- addJobList.php
+++ addJobList.php
@@ -5,23 +5,23 @@ $DB = mysqli_select_db($CN, "job-board");
 $EncodedData=file_get_contents('php://input');
 $DecodedData=json_decode($EncodedData, true);
 
-$name=$DecodedData['name'];
-$employer_id=$DecodedData['employer_id'];
-$description=$DecodedData['description'];
-$salary=$DecodedData['salary'];
-$location=$DecodedData['location'];
-$requirements=$DecodedData['requirements'];
+$name=$_POST['name'];
+$employer_id=$_POST['employer_id'];
+$description=$_POST['description'];
+$salary=$_POST['salary'];
+$location=$_POST['location'];
+$requirements=$_POST['requirements'];
 
 
 $SQL2 = "insert into `job-list` ( name, employer_id, description, salary, location, requirements) values ( '$name', $employer_id, '$description', $salary, '$location', '$requirements' )";
 $R = mysqli_query($CN, $SQL2);
 if($R)
 {
-$Message="registered succefully";
+$Message="registered successfully";
 }
 else
 {
-$Message="registered not succefully";
+$Message="registered not successfully";
 }
 /*}*/
 $Response=$Message;
