593a06ec3ba0348cf2f08df4b94f5f3ab8358240
Aya-Hamdan1 <s11821764@stu.najah.edu>
2023-05-18 23:52:51 +0300

    create delete job

---
+++ deleteJobList.php
@@ -0,0 +1,30 @@
+
+<?php
+// include('db.php');
+$conn = mysqli_connect('localhost', 'root', '');
+$database = mysqli_select_db($conn, 'job-board');
+
+$job_name=$_POST['name'];
+
+
+$SQLU = "select * from `job-list` where name = '$job_name' ";
+$exe = mysqli_query($conn, $SQLU);
+$check =  mysqli_num_rows($exe);
+
+if ($check != 0) {
+    $SQL1 = "delete from `job-list` where name = '$job_name'";
+    $R = mysqli_query($conn, $SQL1);
+    if ($R) {
+        $Message = "Delete Successfully!";
+    } else {
+        $Message = "There are people applying for this job";
+    }
+} 
+else {
+    $Message = "not exist";
+
+}
+	$response[] = array("Message" => $Message);
+
+echo json_encode($response);
+?>
