76c5cbf4db1498ed6676886d1d004de69d6a4b92
Aya-Hamdan1 <s11821764@stu.najah.edu>
2023-05-19 18:39:08 +0300

    Create sign in page

---
+++ signin.php
@@ -0,0 +1,25 @@
+<?php
+// include('db.php');
+$conn = mysqli_connect('localhost', 'root', '');
+$database = mysqli_select_db($conn, 'job-board');
+
+
+
+$email = $_POST['email'];
+$password= $_POST['password']; 
+
+$SQL = "select * from users where email = '$email' and password = '$password'";
+$exeSQL = mysqli_query($conn, $SQL);
+$checkName =  mysqli_num_rows($exeSQL);
+
+if ($checkName == 0) {
+	$Message = 'Email or password is invalid!!!';
+	}
+else {
+        $Message = 'Login success!';
+    }
+
+
+$response = $Message;
+echo json_encode($response);
+?>
