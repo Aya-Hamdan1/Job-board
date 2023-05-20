c88356c1ca49184d165b3358fa4aa63cf7ffe539
Aya-Hamdan1 <s11821764@stu.najah.edu>
2023-05-20 13:35:08 +0300

    Update delete job list

--- deleteJobList.php
+++ deleteJobList.php
@@ -4,15 +4,16 @@
 $conn = mysqli_connect('localhost', 'root', '');
 $database = mysqli_select_db($conn, 'job-board');
 
-$job_name=$_POST['name'];
+// $job_name=$_POST['name'];
+$job_id=$_POST['id'];
 
 
-$SQLU = "select * from `job-list` where name = '$job_name' ";
+$SQLU = "select * from `job-list` where id = '$job_id' ";
 $exe = mysqli_query($conn, $SQLU);
 $check =  mysqli_num_rows($exe);
 
 if ($check != 0) {
-    $SQL1 = "delete from `job-list` where name = '$job_name'";
+    $SQL1 = "delete from `job-list` where id = '$job_id'";
     $R = mysqli_query($conn, $SQL1);
     if ($R) {
         $Message = "Delete Successfully!";
